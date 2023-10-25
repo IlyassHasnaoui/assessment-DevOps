84e27d507fdfd251d7e630699b69ae058e0edad1
“Ilyass <ilyass.has@live.fr>
2023-10-25 19:07:23 -0400

    new assessment

---
+++ __tests__/shuffle.test.js
@@ -0,0 +1,28 @@
+const shuffle = require("../src/shuffle");
+
+describe("shuffle should...", () => {
+  // CODE HERE
+  test("returns an array with the same length as the array provided.", async () => {
+    
+    let abcArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    let result = shuffle(abcArr)
+    expect(result.length).toEqual(10)
+  });
+
+  test("can the shuffle function shuffle the items in the array provided.", async () => {
+
+    let abcArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    let result = shuffle(abcArr)
+    expect(abcArr).not.toEqual(result)
+
+  });
+
+  test("are the items the same as in the array provided.", async () => {
+
+    let abcArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    let result = shuffle(abcArr)
+    expect(result).toContain('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+
+  });
+
+});
